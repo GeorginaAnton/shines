05146daef1dc91d8555a450cf0d05ed415d30c73
Georgina Anton <georginasusana30@gmail.com>
2023-12-01 10:07:12 -0500

    mi primer commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+console.log("Hola mundo");
+console.log("Mi nombre es gina");
+console.log("Adios");
+
+var miTitulo ="mi primera pagina";
+
+console.log(miTitulo)
+
+ var miEtiquetaTitulo = document.getElementById("title");
+
+ console.log(miEtiquetaTitulo);
+ console.log(miEtiquetaTitulo.firstChild);
+
+ var listaDeEtiquetas = document.getElementsByTaName("a");
+
+console.log(listaDeEtiquetas);
+console.log(listaDeEtiquetas 2);
+
+var lista2 = document.getElementsByClassName("item");
+console.log(lista2);
+console.log(lista2);
